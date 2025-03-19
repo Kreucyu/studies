8fd5caa1f94e5cc70bffc3ce75eedd2e446fef27
Kreucyu <elciocleiton72@gmail.com>
2025-03-19 09:36:10 -0300

    question 3 resolve

---
+++ collegeSubjects/thirdSemester/algorithmAndDataStructure/question3/src/model/noLista.java
@@ -0,0 +1,23 @@
+package model;
+
+public class noLista<T> {
+	private T info;
+	private noLista<T> proximo;
+
+	public T getInfo() {
+		return info;
+	}
+
+	public void setInfo(T info) {
+		this.info = info;
+	}
+	
+	public noLista<T> getProximo() {
+		return proximo;
+	}
+
+	public void setProximo(noLista<T> proximo) {
+		this.proximo = proximo;
+	}
+	
+}
