c05f2103b3bf7963ef8807f81dd9732ecf7766e9
Kreucyu <elciocleiton72@gmail.com>
2025-04-15 19:40:52 -0300

    advanced db

---
+++ collegeSubjects/thirdSemester/algorithmAndDataStructure/sd/src/sd/asd.java
@@ -0,0 +1,16 @@
+package sd;
+
+public class asd {
+
+	public static void main(String[] args) {
+	
+		Algoritmo: inserir(int valor) 
+		NoListaDupla novo = new NoListaDupla();
+		novo.info = valor;
+		novo.proximo = primeiro;
+		novo.anterior = null;
+		se (primeiro != null) {
+			-----------
+		}
+		------------------
+}
