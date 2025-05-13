e945c00c3339bacc147aaeb88b7da0a41b9b78f0
Kreucyu <elciocleiton72@gmail.com>
2025-05-13 14:52:51 -0300

    sorts

---
+++ collegeSubjects/thirdSemester/algorithmAndDataStructure/sorts/src/entities/OrdenacaoAbstract.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class OrdenacaoAbstract<T extends Comparable<T>> {
+	private T[] info;
+
+	public T[] getInfo() {
+		return info;
+	}
+
+	public void setInfo(T[] info) {
+		this.info = info;
+	}
+	
+	public void trocar(int a, int b) {
+		
+	}
+	
+	public void ordenar() {
+		
+	}
+}
---
+++ collegeSubjects/thirdSemester/algorithmAndDataStructure/sorts/src/entities/OrdenacaoBolhaOtimizada.java
@@ -0,0 +1,5 @@
+package entities;
+
+public class OrdenacaoBolhaOtimizada<T extends Comparable<T>> {
+
+}
