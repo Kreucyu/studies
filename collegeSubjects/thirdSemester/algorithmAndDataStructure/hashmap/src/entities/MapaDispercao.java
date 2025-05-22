51ba4fb0ba0b55ea46e7a8aaa01fc293c273fd5f
Kreucyu <elciocleiton72@gmail.com>
2025-05-21 21:20:33 -0300

    hashmap

---
+++ collegeSubjects/thirdSemester/algorithmAndDataStructure/hashmap/src/entities/MapaDispercao.java
@@ -0,0 +1,6 @@
+package entities;
+
+public class MapaDispercao<T> {
+	private ListaEncadeada<NoMapa<T>> info[];
+	
+}
---
+++ collegeSubjects/thirdSemester/algorithmAndDataStructure/hashmap/src/entities/NoMapa.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class NoMapa<T> {
+private int chave;
+private T valor;
+
+public int getChave() {
+	return chave;
+	
+}
+
+public void setChave(int chave) {
+	
+}
+
+public T getValor() {
+	return valor;	 
+}
+
+public void setValor(T valor) {
+	
+}
+
+public boolean equals(Object o) {
+	return false;
+	
+}
+
+}
